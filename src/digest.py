3937ddf211116e6d3a94b8d666d8b77929316586
tkessinger <taylor.kessinger@tuebingen.mpg.de>
2012-06-20 19:34:48 +0200

    Initial commit

---
+++ src/digest.py
@@ -0,0 +1,11 @@
+'''
+author:     Taylor Kessinger
+date:       18/06/12
+content:    Digest a specified protein sequence as if trypsin were the protease.
+            Output (to STDOUT) fragments of size 5 or more.
+'''
+
+def trypsin_digest(infile, minlength=5):
+    
+    
+    pass
